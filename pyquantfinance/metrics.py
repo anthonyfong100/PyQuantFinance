2e0ec2e6457bfbbc9ad678ab1539b555f86ba910
Anthony <anthonyfong100@gmail.com>
2020-04-18 21:38:04 +0800

    implemented code for var , cvar, sharpe ratio, semideviation
    Implemented code to calculate value at risk, conditional value at risk, sharpe ratio and semideviation.

--- pyquantfinance/metrics.py
+++ pyquantfinance/metrics.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import numpy as np
 import scipy
 from typing import Union
+from scipy.stats import norm
 from .decorators import accepts
+from .tvm import annualize_rets, annualize_vol
 
 
 @accepts((list, pd.Series))
@@ -75,3 +78,78 @@ def is_normal(r, level=0.01, method="jb"):
         else:
             raise ValueError(
                 "Method not implemented, currently only support jb (Jarque Bera test)")
+
+
+@accepts(pd.Series, float, int)
+def sharpe_ratio(returns: pd.Series, riskfree_rate: float,
+                 periods_per_year: int) -> float:
+    """
+    Computes the annualized sharpe ratio of a set of returns expressed as a series.
+    riskfree_rate is expressed as a decimal (e.g 40% = 0.4)
+    period_per_year is the number of times returns compounded per year
+    """
+    # convert the annual riskfree rate to per period
+    rf_per_period = (1 + riskfree_rate)**(1 / periods_per_year) - 1
+    excess_returns = returns - rf_per_period
+    ann_ex_returns = annualize_rets(excess_returns, periods_per_year)
+    ann_vol = annualize_vol(returns, periods_per_year)
+    return ann_ex_returns / ann_vol
+
+
+@accepts((pd.Series, pd.DataFrame))
+def semi_deviation(returns: Union[pd.Series, pd.DataFrame]
+                   ) -> Union[pd.Series, pd.DataFrame]:
+    """
+    Returns the semideviation aka negative semideviation of returns
+    """
+    if isinstance(returns, pd.Series):
+        is_negative = returns < 0  # create a mask to select entries with return < 0
+        return returns[is_negative].std(ddof=0)
+    else:
+        return returns.aggregate(semi_deviation)
+
+
+@accepts((pd.Series, pd.DataFrame), int, str)
+def value_at_risk(returns: Union[pd.Series, pd.DataFrame],
+                  level: int = 5, method="historic") -> Union[float, pd.Series]:
+    """
+    Returns the Value at Risk (percent always positive) at a specified level
+    level specifies the confidence level which we want to adopt
+    method = historic , gaussian or gaussian_cf (cornish fisher)
+    i.e. returns the number such that "level" percent of the returns
+    fall below that number, and the (100-level) percent are above
+    """
+    if isinstance(returns, pd.DataFrame):
+        return returns.aggregate(value_at_risk, level=level, method=method)
+    else:
+        if method == "historic":
+            return abs(np.percentile(returns, level))
+        elif method == "gaussian":
+            z = norm.ppf(level / 100)
+            return -(returns.mean() + z * returns.std(ddof=0))
+        elif method == "gaussian_cf":
+            s = skewness(returns)
+            k = kurtosis(returns)
+            z = (z +
+                 (z**2 - 1) * s / 6 +
+                 (z**3 - 3 * z) * (k - 3) / 24 -
+                 (2 * z**3 - 5 * z) * (s**2) / 36
+                 )
+            return -(returns.mean() + z * returns.std(ddof=0))
+        else:
+            raise ValueError(
+                "Method supports only include historic, gaussian or gaussian_cf")
+
+
+@accepts((pd.Series, pd.DataFrame), int)
+def cond_value_at_risk(returns: Union[pd.Series, pd.DataFrame],
+                       level: int = 5) -> Union[float, pd.Series]:
+    """
+    Computes the Conditional VaR of Series or DataFrame
+    """
+    if isinstance(returns, pd.Series):
+        # only take those entries with returns < value_at_risk
+        is_beyond = returns <= value_at_risk(returns, level=level)
+        return -returns[is_beyond].mean()
+    else:
+        return returns.aggregate(cond_value_at_risk, level=level)
